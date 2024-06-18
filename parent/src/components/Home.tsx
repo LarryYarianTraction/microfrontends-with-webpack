4b0b65f8b17095c021d4372e724d2aeead3b5e2c
Nick Jackson <nick@tractionag.com>
2024-06-17 21:01:32 -0400

    Replace useEffect with useSessionStore

--- parent/src/components/Home.tsx
+++ parent/src/components/Home.tsx
@@ -3,27 +3,28 @@ import {
 	Input,
 	Text
 } from '@chakra-ui/react'
-import { FunctionComponent, ReactElement, useState } from 'react'
+import { FunctionComponent, ReactElement, useEffect, useState } from 'react'
+import { useSessionStore } from '../stores/sessionStore'
 
 export const Home: FunctionComponent = (): ReactElement => {
-	const [search, setSearch] = useState('')
+	const { search: setSessionStoreSearch, searchText } = useSessionStore()
 
-	const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setSearch(event.target.value)
+	const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSessionStoreSearch(event.target.value)
 	}
 
 	return (
 		<Box>
 			<Input
 				placeholder="Search..."
-				value={search}
-				onChange={handleSearchInputChange}
+				value={searchText}
+				onChange={handleSearch}
 				width='50%'
 				backgroundColor='white'
 				my={4}
 			/>
 			<Text mt={2}>
-				{search}
+				{searchText}
 			</Text>
 		</Box>
 	)
